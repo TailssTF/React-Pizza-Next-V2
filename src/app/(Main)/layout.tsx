211627dffe6aaa5a5061d89bfbae2e9d2cbb5e62
TailssTF <tailss5@mail.ru>
2024-04-23 14:03:11 +0700

    feat: добавил компонент модалки

--- src/app/(Main)/layout.tsx
+++ src/app/(Main)/layout.tsx
@@ -5,13 +5,23 @@ import { Header } from "@/components";
 import { StoreContext } from "../../Store-context";
 import Store from "@/stores/Store";
 
-const MainLayout = ({ children }: { children: React.ReactNode }) => {
+const MainLayout = ({
+  modal,
+  children,
+}: {
+  modal: React.ReactNode;
+  children: React.ReactNode;
+}) => {
   return (
     <StoreContext.Provider value={new Store()}>
       <div className="wrapper">
         <Header />
         <main className="content">
-          <Suspense fallback={<div>Идет загрузка...</div>}>{children}</Suspense>
+          <Suspense fallback={<div>Идет загрузка...</div>}>
+            <div id="modal-root" />
+            {children}
+            {modal}
+          </Suspense>
         </main>
       </div>
     </StoreContext.Provider>
