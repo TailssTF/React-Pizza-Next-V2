3cfae60e19373edfcf8f1fb5a254f7ba132069ea
TailssTF <tailss5@mail.ru>
2024-05-21 16:40:53 +0700

    fix(Categoty): добавил использование хука

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,19 +1,17 @@
 "use client";
 import { allCategories } from "@/constants";
-import { usePathname, useSearchParams, useRouter } from "next/navigation";
+import { useQueryParams } from "@/utils/useQueryParams";
+import { useSearchParams } from "next/navigation";
 import React from "react";
 
 export const Categories = () => {
   const searchParams = useSearchParams();
-  const pathname = usePathname();
-  const { replace } = useRouter();
   const params = new URLSearchParams(searchParams);
   const category = Number(params.get("category"));
+  const { setQueryParams } = useQueryParams();
 
   const handleChangeCategory = (index: number) => {
-    params.set("category", String(index));
-    params.set("page", "0");
-    replace(`${pathname}?${params.toString()}`);
+    setQueryParams({ category: index, page: 0 });
   };
 
   return (
